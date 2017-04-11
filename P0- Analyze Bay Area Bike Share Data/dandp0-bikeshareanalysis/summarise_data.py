be670bda6461de1b944a122dcafa0914c8685e24
sushgandhi <tosushantgandhi@gmail.com>
2017-04-11 23:15:34 +0530

    Initial Commit

---
+++ P0- Analyze Bay Area Bike Share Data/dandp0-bikeshareanalysis/summarise_data.py
@@ -0,0 +1,55 @@
+def summarise_data(trip_in, station_data, trip_out):
+    """
+    This function takes trip and station information and outputs a new
+    data file with a condensed summary of major trip information. The
+    trip_in and station_data arguments will be lists of data files for
+    the trip and station information, respectively, while trip_out
+    specifies the location to which the summarized data will be written.
+    """
+    # generate dictionary of station - city mapping
+    station_map = create_station_mapping(station_data)
+    
+    with open(trip_out, 'w') as f_out:
+        # set up csv writer object        
+        out_colnames = ['duration', 'start_date', 'start_year',
+                        'start_month', 'start_hour', 'weekday',
+                        'start_city', 'end_city', 'subscription_type']        
+        trip_writer = csv.DictWriter(f_out, fieldnames = out_colnames)
+        trip_writer.writeheader()
+        
+        for data_file in trip_in:
+            with open(data_file, 'r') as f_in:
+                # set up csv reader object
+                trip_reader = csv.DictReader(f_in)
+
+                # collect data from and process each row
+                for row in trip_reader:
+                    new_point = {}
+                    
+                    # convert duration units from seconds to minutes
+                    ### Question 3a: Add a mathematical operation below   ###
+                    ### to convert durations from seconds to minutes.     ###
+                    
+                    new_point['duration'] = float(row['Duration'])/60
+                    
+                    # reformat datestrings into multiple columns
+                    ### Question 3b: Fill in the blanks below to generate ###
+                    ### the expected time values.                         ###
+                    trip_date = datetime.strptime(row['Start Date'], '%m/%d/%Y %H:%M')
+                    new_point['start_date']  = trip_date.strftime('%Y-%m-%d')
+                    new_point['start_year']  = trip_date.strftime('%Y')
+                    new_point['start_month'] = trip_date.strftime('%m')
+                    new_point['start_hour']  = trip_date.strftime('%H')
+                    new_point['weekday']     = trip_date.strftime('%A')
+                    
+                    # remap start and end terminal with start and end city
+                    new_point['start_city'] = station_map[row['Start Terminal']]
+                    new_point['end_city'] = station_map[row['End Terminal']]
+                    # two different column names for subscribers depending on file
+                    if 'Subscription Type' in row:
+                        new_point['subscription_type'] = row['Subscription Type']
+                    else:
+                        new_point['subscription_type'] = row['Subscriber Type']
+
+                    # write the processed information to the output file.
+                    trip_writer.writerow(new_point)  
